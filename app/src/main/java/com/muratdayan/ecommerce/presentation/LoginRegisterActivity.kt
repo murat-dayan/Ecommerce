450d29b34b3c374f4b36793070af67fae1835660
muratdayan <muratdayan359@gmail.com>
2024-07-07 13:37:08 +0300

    package structure created

--- app/src/main/java/com/muratdayan/ecommerce/presentation/LoginRegisterActivity.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/LoginRegisterActivity.kt
@@ -1,16 +1,17 @@
-package com.muratdayan.ecommerce
+package com.muratdayan.ecommerce.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.muratdayan.ecommerce.R
 
-class MainActivity : AppCompatActivity() {
+class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
