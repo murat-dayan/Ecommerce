192eeafa32f48004be3a52a9294a9012bebb7a1c
muratdayan <muratdayan359@gmail.com>
2024-07-08 14:58:00 +0300

    di module created and return firebaseauth and application class created

---
+++ app/src/main/java/com/muratdayan/ecommerce/EcommerceApplication.kt
@@ -0,0 +1,8 @@
+package com.muratdayan.ecommerce
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EcommerceApplication : Application() {
+}
--- app/src/main/java/com/muratdayan/ecommerce/presentation/register/RegisterViewModel.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/register/RegisterViewModel.kt
@@ -2,8 +2,11 @@ package com.muratdayan.ecommerce.presentation.register
 
 import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.muratdayan.ecommerce.data.model.User
+import com.muratdayan.ecommerce.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
@@ -12,13 +15,18 @@ class RegisterViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
-    private val _register = MutableStateFlow<>()
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    val register : Flow<Resource<FirebaseUser>> = _register
 
     fun createAccountWithEmailAndPassword(user: User,password:String){
         firebaseAuth.createUserWithEmailAndPassword(user.email,password)
-            .addOnSuccessListener {
+            .addOnSuccessListener {result ->
+                result.user?.let {firebaseUser->
+                    _register.value = Resource.Success(firebaseUser)
+                }
 
-            }.addOnFailureListener {
+            }.addOnFailureListener {result->
+                _register.value = Resource.Error(result.message.toString())
 
             }
     }
