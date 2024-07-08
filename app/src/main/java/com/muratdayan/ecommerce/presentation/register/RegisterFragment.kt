f1e0ea7ecb96250f0e70f7c4bd9084c6f29ef24a
muratdayan <muratdayan359@gmail.com>
2024-07-08 15:07:20 +0300

    added register with firebaseemailandpassword in RegisterFragment

--- app/src/main/java/com/muratdayan/ecommerce/presentation/register/RegisterFragment.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/register/RegisterFragment.kt
@@ -1,7 +1,68 @@
 package com.muratdayan.ecommerce.presentation.register
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.muratdayan.ecommerce.R
+import com.muratdayan.ecommerce.data.model.User
+import com.muratdayan.ecommerce.databinding.FragmentRegisterBinding
+import com.muratdayan.ecommerce.util.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+private val TAG = "registerFragment"
+
+@AndroidEntryPoint
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    private lateinit var binding: FragmentRegisterBinding
+    private val registerViewModel by viewModels<RegisterViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            btnRegisterRegister.setOnClickListener {
+                val user = User(
+                    etFirstNameRegister.text.toString().trim(),
+                    etLastNameRegister.text.toString().trim(),
+                    etEmailRegister.text.toString().trim()
+                )
+                val password = etPasswordRegister.text.toString()
+                registerViewModel.createAccountWithEmailAndPassword(user,password)
+            }
+        }
+
+        lifecycleScope.launch {
+            registerViewModel.register.collect{result->
+                when(result){
+                    is Resource.Error -> {
+                        Log.e(TAG,result.message.toString())
+                        binding.btnRegisterRegister.revertAnimation()
+                    }
+                    is Resource.Loading -> {
+                        binding.btnRegisterRegister.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        Log.d("test",result.data.toString())
+                        binding.btnRegisterRegister.revertAnimation()
+                    }
+                }
+            }
+        }
+    }
 }
