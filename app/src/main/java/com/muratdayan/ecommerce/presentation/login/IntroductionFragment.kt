efc521ace7eb30443d63a633358244be82371f0a
muratdayan <muratdayan359@gmail.com>
2024-07-09 16:17:03 +0300

    IntroductionFragment setup

--- app/src/main/java/com/muratdayan/ecommerce/presentation/login/IntroductionFragment.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/login/IntroductionFragment.kt
@@ -1,9 +1,34 @@
 package com.muratdayan.ecommerce.presentation.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.muratdayan.ecommerce.R
+import com.muratdayan.ecommerce.databinding.FragmentIntroductionBinding
 
 class IntroductionFragment : Fragment(R.layout.fragment_introduction) {
+
+    private lateinit var binding: FragmentIntroductionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentIntroductionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnIntroductionStart.setOnClickListener {
+            findNavController().navigate(R.id.navigate_introductionFragment_to_accountOptionsFragment)
+        }
+    }
 
 
 }
