f473a715efbd2e1ea598664f244e1ac4ae8ea1d6
muratdayan <muratdayan359@gmail.com>
2024-07-09 17:22:45 +0300

    dialog styled for reset password and extensin func created for inflate and buttons

---
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/dialog/ResetPasswordDialog.kt
@@ -0,0 +1,32 @@
+package com.muratdayan.ecommerce.presentation.dialog
+
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.muratdayan.ecommerce.R
+
+fun Fragment.setUpBottomSheetDialog(
+    onSendClick: (String) -> Unit,
+){
+
+    val dialog = BottomSheetDialog(requireContext())
+    val view = layoutInflater.inflate(R.layout.reset_password_dialog, null)
+    dialog.setContentView(view)
+    dialog.show()
+
+    val etEmail = view.findViewById<EditText>(R.id.etResetPassword)
+    val buttonSend = view.findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnSendResetPasswordDialog)
+    val buttonCancel = view.findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnCancelResetPasswordDialog)
+
+    buttonSend.setOnClickListener {
+        val email = etEmail.text.toString().trim()
+        onSendClick(email)
+    }
+
+    buttonCancel.setOnClickListener {
+        dialog.dismiss()
+    }
+
+
+
+}
