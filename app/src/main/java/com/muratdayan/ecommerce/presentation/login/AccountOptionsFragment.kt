cac4d2287caafd0511c3d346989308efa18d1fe5
muratdayan <muratdayan359@gmail.com>
2024-07-09 16:18:50 +0300

    AccountOptionsFragment setup

--- app/src/main/java/com/muratdayan/ecommerce/presentation/login/AccountOptionsFragment.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/login/AccountOptionsFragment.kt
@@ -1,7 +1,36 @@
 package com.muratdayan.ecommerce.presentation.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.muratdayan.ecommerce.R
+import com.muratdayan.ecommerce.databinding.FragmentAccountOptionsBinding
 
 class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+
+    private lateinit var binding: FragmentAccountOptionsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentAccountOptionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLoginAccountOptions.setOnClickListener {
+            findNavController().navigate(R.id.navigate_accountOptionsFragment_to_loginFragment)
+        }
+
+        binding.btnRegisterAccountOptions.setOnClickListener {
+            findNavController().navigate(R.id.navigate_accountOptionsFragment_to_registerFragment)
+        }
+    }
 }
