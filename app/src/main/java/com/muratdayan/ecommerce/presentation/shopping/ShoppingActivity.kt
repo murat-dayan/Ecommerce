031f8038c6a7cb301964a76ee540c432adf189ae
muratdayan <muratdayan359@gmail.com>
2024-07-10 19:32:55 +0300

    shoppinggraph hosted by ShoppingActivity

--- app/src/main/java/com/muratdayan/ecommerce/presentation/shopping/ShoppingActivity.kt
+++ app/src/main/java/com/muratdayan/ecommerce/presentation/shopping/ShoppingActivity.kt
@@ -5,17 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.play.integrity.internal.f
 import com.muratdayan.ecommerce.R
+import com.muratdayan.ecommerce.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavView.setupWithNavController(navController)
+
     }
 }
